93a7b278d34158f9b78dfcaf0caae3f9b06a3636
Filip Suster <filip.suster@t-mobile.cz>
2021-12-31 13:58:03 +0100

    Implementing PR review remarks

--- src/section_handlers/document_tracking.py
+++ src/section_handlers/document_tracking.py
@@ -1,8 +1,11 @@
 import logging
 import re
+import sys
 
+from typing import Tuple
 from src.common.common import SectionHandler
 from utils import get_utc_timestamp
+from operator import itemgetter
 
 
 class DocumentTracking(SectionHandler):
@@ -17,7 +20,7 @@ class DocumentTracking(SectionHandler):
         self.json['initial_release_date'] = root_element.InitialReleaseDate.text
         self.json['status'] = root_element.Status.text
 
-        revision_history, version = DocumentTracking._process_revision_history(root_element)
+        revision_history, version = self._process_revision_history(root_element)
         self.json['revision_history'] = revision_history
         self.json['version'] = version
 
@@ -28,14 +31,23 @@ class DocumentTracking(SectionHandler):
         self.json['generator']['engine']['name'] = self.cvrf2csaf_name
         self.json['generator']['engine']['version'] = self.cvrf2csaf_version
 
-    """
-    Checks whether all version numbers in /document/tracking/revision_history match semantic versioning.
-    semantic version is defined in version_t definition
-    see: https://docs.oasis-open.org/csaf/csaf/v2.0/csd01/csaf-v2.0-csd01.html#3111-version-type and
-    section 9.1.5 Conformance Clause 5: CVRF CSAF converter
-    """
+    def _process_optional_elements(self, root_element):
+        if hasattr(root_element.Identification, 'Alias'):
+            aliases = []
+            for alias in root_element.Identification.Alias:
+                aliases.append(alias.text)
+
+            self.json['aliases'] = aliases
+
     @staticmethod
     def check_for_version_t(revision_history):
+        """
+        Checks whether all version numbers in /document/tracking/revision_history match semantic versioning.
+        semantic version is defined in version_t definition
+        see: https://docs.oasis-open.org/csaf/csaf/v2.0/csd01/csaf-v2.0-csd01.html#3111-version-type and
+        section 9.1.5 Conformance Clause 5: CVRF CSAF converter
+        """
+
         pattern = (
             r'^((0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)'
             r'(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?)$'
@@ -43,7 +55,14 @@ class DocumentTracking(SectionHandler):
         return all(re.match(pattern, revision['number']) for revision in revision_history)
 
     @staticmethod
-    def _process_revision_history(root_element):
+    def _as_int_tuple(text: str) -> Tuple[int]:
+        """ Converts string of dotted numbers into tuple of ints """
+        try:
+            return tuple(int(part) for part in text.split('.'))
+        except ValueError:
+            return (sys.maxsize, )
+
+    def _process_revision_history(self, root_element):
         # preprocess the data
         revision_history = []
         for revision in root_element.RevisionHistory.Revision:
@@ -52,38 +71,33 @@ class DocumentTracking(SectionHandler):
             revision_history.append(
                 {
                     'date': revision.Date.text,
-                    'number_cvrf': revision.Number.text,
                     'number': revision.Number.text,
                     'summary': revision.Description.text,
+                    # Extra vars
+                    'number_cvrf': revision.Number.text,
+                    'version_as_int_tuple': self._as_int_tuple(revision.Number.text),
                 }
             )
 
         # handle corresponding part of Conformance Clause 5: CVRF CSAF converter
         # that is: some version numbers in revision_history don't match semantic versioning
-        if not DocumentTracking.check_for_version_t(revision_history):
+        if not self.check_for_version_t(revision_history):
             logging.info('Some version numbers in /document/tracking/revision_history does not match semantic versioning. Reindexing to integers.')
-            # Sort revisions by version number
-            # TODO: Double check if validation of input document is enough or shall we handle potential exceptions here
-            revision_history = sorted(revision_history, key=lambda x: list(map(int, x['number'].split('.'))))
 
-            for renumber, revision in enumerate(revision_history, start=1):
-                revision['number'] = renumber  # Changing the type from str to int
+            revision_history = sorted(revision_history, key=itemgetter('version_as_int_tuple'))
+
+            for rev_number, revision in enumerate(revision_history, start=1):
+                revision['number'] = rev_number  # Changing the type from str to int
 
         # match document version to corresponding one in revision history
         # TODO: maybe throw exception if more than one revision matches? Is it checked somewhere else? see: http://docs.oasis-open.org/csaf/csaf-cvrf/v1.2/cs01/csaf-cvrf-v1.2-cs01.html#_Toc493508877
-        current_revision = list(filter(lambda revision: revision['number_cvrf'] == root_element.Version.text, revision_history))[0]
-        version = current_revision['number']
+        version = next(rev for rev in revision_history if rev['number_cvrf'] == root_element.Version.text)['number']
 
         # cleanup extra vars
         for revision in revision_history:
             revision.pop('number_cvrf')
+            revision.pop('version_as_int_tuple')
 
         return revision_history, version
 
-    def _process_optional_elements(self, root_element):
-        if hasattr(root_element.Identification, 'Alias'):
-            aliases = []
-            for alias in root_element.Identification.Alias:
-                aliases.append(alias.text)
 
-            self.json['aliases'] = aliases
