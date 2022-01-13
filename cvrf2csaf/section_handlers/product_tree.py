44b3064ab81722651a71ecac2d19fa1c470ad29a
Filip Suster <filip.suster@t-mobile.cz>
2022-01-13 14:14:01 +0100

    WIP: Product Tree: Relationships

--- cvrf2csaf/section_handlers/product_tree.py
+++ cvrf2csaf/section_handlers/product_tree.py
@@ -1,11 +1,5 @@
 import logging
-import re
-import sys
-from operator import itemgetter
-from typing import Tuple
 from ..common.common import SectionHandler
-from ..common.utils import get_utc_timestamp
-
 
 
 class ProductTree(SectionHandler):
@@ -19,21 +13,42 @@ class ProductTree(SectionHandler):
         if hasattr(root_element, 'FullProductName'):
             full_product_names = []
             for full_product_name in root_element.FullProductName:
-                full_product_names.append({
+                fpn_to_add = {
                     'product_id': full_product_name.attrib['ProductID'],
                     'name': full_product_name.text,
-                    'product_identification_helper': {'cpe': full_product_name.attrib['CPE']},
-                })
+                }
+                if full_product_name.attrib.get('CPE'):
+                    fpn_to_add['product_identification_helper'] = {'cpe': full_product_name.attrib['CPE']}
+
+                full_product_names.append(fpn_to_add)
 
             self.csaf['full_product_names'] = full_product_names
 
-        # if hasattr(root_element, 'Relationship'):
-        #     relationships = []
-        #     for relationship in root_element.Relationship:
-        #         relationships.append({
-        #             # optional CPE attribute in CVRF is omitted (not present in CSAF specification)
-        #             # 'product_id': full_product_name.attrib['ProductID'],
-        #             # 'name': full_product_name.text,
-        #         })
-        #
-        #     self.csaf['relationships'] = relationships
+        if hasattr(root_element, 'Relationship'):
+            relationships = []
+            for relationship in root_element.Relationship:
+                first_prod_name = relationship.FullProductName[0]
+
+                if len(relationship.FullProductName) > 1:
+                    # To be compliant with 9.1.5 Conformance Clause 5: CVRF CSAF converter
+                    # https://docs.oasis-open.org/csaf/csaf/v2.0/csaf-v2.0.html
+                    logging.warning(f'Input line {relationship.sourceline}: Relationship contains more '
+                                    'FullProductNames. Taking only the first one, since CSAF expects '
+                                    'only 1 value here')
+
+                rel_to_add = {
+                    'category': relationship.attrib['RelationType'],
+                    'product_reference': relationship.attrib['ProductReference'],
+                    'relates_to_product_reference': relationship.attrib['RelatesToProductReference'],
+                    'full_product_name': {
+                        'product_id': first_prod_name.attrib['ProductID'],
+                        'name': first_prod_name.text,
+                    }
+                }
+
+                if first_prod_name.attrib.get('CPE'):
+                    rel_to_add['full_product_name']['product_identification_helper'] = {'cpe': first_prod_name.attrib['CPE']}
+
+                relationships.append(rel_to_add)
+
+                self.csaf['relationships'] = relationships
