c6c63019e4e9aa7723381b812398a074f36ec042
cgi <cgi@gitlab.com>
2022-01-18 09:46:41 +0100

    Implementing document_notes.py #35

--- cvrf2csaf/section_handlers/document_notes.py
+++ cvrf2csaf/section_handlers/document_notes.py
@@ -1,13 +1,50 @@
 import logging
 from ..common.common import SectionHandler
+from ..common.utils import etree_to_dict
 
 
 class DocumentNotes(SectionHandler):
     def __init__(self):
         super().__init__()
 
+        self.enum_categories = [
+            'description',
+            'details',
+            'faq',
+            'general',
+            'legal_disclaimer',
+            'other',
+            'summary'
+        ]
+
+    def _process_mandatory_and_optional(self, root_element):
+        notes = []
+
+        for elem_note in root_element.Note:
+
+            # mandatory
+            new_note = dict(
+                text=elem_note.text,
+                category=elem_note.get('Type'),
+            )
+
+            if new_note['category'].lower() not in self.enum_categories:
+                logging.warn(f'Invalid document notes category '
+                             f'{new_note["category"]}. Should be one of {self.enum_categories}!')
+
+            # optional
+            if elem_note.get('Audience'):
+                new_note['audience'] = elem_note.get('Audience')
+            if elem_note.get('Title'):
+                new_note['title'] = elem_note.get('Title')
+
+            notes.append(new_note)
+
+        if len(notes) > 0:
+            self.csaf['notes'] = notes
+
     def _process_mandatory_elements(self, root_element):
-        logging.warning(f'Not implemented input {self.__class__}. No parser written.')
+        return self._process_mandatory_and_optional(root_element=root_element)
 
     def _process_optional_elements(self, root_element):
-        pass
+        pass # Not used anymore.
