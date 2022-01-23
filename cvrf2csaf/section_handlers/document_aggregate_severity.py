98ab751bdc820b216b53d16caf6bf7cbc4bb5c12
cgi <cgi@gitlab.com>
2022-01-23 22:56:59 +0100

    #39 stub implement.

--- cvrf2csaf/section_handlers/document_aggregate_severity.py
+++ cvrf2csaf/section_handlers/document_aggregate_severity.py
@@ -6,8 +6,18 @@ class AggregateSeverity(SectionHandler):
     def __init__(self):
         super().__init__()
 
-    def _process_mandatory_elements(self, root_element):
+    def _process_mandatory_and_optional(self, root_element):
+
         logging.warning(f'Not implemented input {self.__class__}. No parser written.')
+        # 39 -> To be tested! No example in input for now.
+        csaf_dict = dict(
+            namespace=root_element.get('Namespace'),
+            text=root_element.text,
+        )
+        self.csaf = csaf_dict
+
+    def _process_mandatory_elements(self, root_element):
+        self._process_mandatory_and_optional(root_element=root_element)
 
     def _process_optional_elements(self, root_element):
         pass
