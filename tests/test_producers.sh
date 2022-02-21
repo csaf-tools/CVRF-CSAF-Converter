df8a12925f3542d7649367e15633322a08a72a4e
Filip Suster <filip.suster@t-mobile.cz>
2022-02-21 13:21:37 +0100

    Alter the test_producers script

--- tests/test_producers.sh
+++ tests/test_producers.sh
@@ -5,16 +5,19 @@ cd ..
 mkdir outdir 2>/dev/null
 
 #subdirs=("1.2" "examples_cisco" "examples_redhat")
-subdirs=("examples_cisco")
+subdirs=("examples_oracle")
 
+c=0
 for subdir in ${subdirs[@]}; do
   FILES=$(find examples/$subdir -type f)
 
   for f in $FILES; do
       echo -e "------------------- \n -------------------- \n"
       echo "INPUT FILE: $f "
-      cvrf2csaf --force --input-file $f --output-file outdir/$(basename $f).json 2>&1 |grep "CSAF schema validation OK"
+      cvrf2csaf --force --input-file $f --output-file outdir/$(basename $f).json
 
 #      read
+      let c=$((c+=1))
+      [[ $c -gt 100 ]] && break;
   done
 done
